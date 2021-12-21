a194c1070341443308c9cab51df166aff4e31849
root <root@ik1-103-58245.vs.sakura.ne.jp>
2021-12-21 10:51:18 +0900

    MGSサイトからAV女優データのスクレイピング

--- app/Console/Commands/Scraping.php
+++ app/Console/Commands/Scraping.php
@@ -42,38 +42,53 @@ class Scraping extends Command
     public function handle()
     {
         
-        $targetUrl = 'https://www.dmm.co.jp/digital/videoa/-/actress/=/keyword=wa/sort=count/';
+        //$targetUrl = 'https://www.dmm.co.jp/digital/videoa/-/actress/=/keyword=wa/sort=count/';
+        //$targetUrl = 'https://www.mgstage.com/?form=prestige_18kin&utm_medium=official&utm_source=prestige&utm_campaign=prestige_18kin&agef=1';
+        //$targetUrl = 'https://www.prestige-av.com/';
+
+        $cookies = ['uuid', '38dbd6482dfd41c1002f71bd9f3862af'];
+
         $client = new \Goutte\Client();
-        $goutte = GoutteFacade::request('GET', $targetUrl);
-        $link = $goutte->selectLink("はい")->link();
+
+        $client->getCookieJar()->updateFromSetCookie($cookies);
+
+        $targetUrl = 'https://www.mgstage.com/?form=prestige_18kin&utm_medium=official&utm_source=prestige&utm_campaign=prestige_18kin&agef=1';
+
+        $crawler = $client->request('GET', $targetUrl);
+
+
+        $link = $crawler->selectLink('女優一覧へ')->link();
+
         $clicked = $client->click($link);
 
-
-        $div = $clicked->filter('div.act-box');
-        $div->filter('li')->each(function($li) {
-            $sexy_actress = new SexyActress();
-            $sexy_actress->name = $li->filter('img')->attr('alt');
-            $sexy_actress->image_name = $li->filter('img')->attr('src');
-            $sexy_actress->purchase_link = $li->filter('a')->attr('href');
+        $clicked->filter('div.push_actress')->each(function ($div) {
+            
+            $div->filter('li')->each(function ($li) {
+                $sexy_actress = new SexyActress();
+                $sexy_actress->name = $li->text();
+                $sexy_actress->purchase_link = $li->filter('a')->attr('href');
+                $sexy_actress->image_name = $li->filter('img')->attr('src');
+                
         
-            DB::beginTransaction();
-        try {
-            $sexy_actress->save();
-            DB::commit();
-            Log::notice('スクレイピングバッチの実行が成功しました。');
+                DB::beginTransaction();
+                try {
+                    $sexy_actress->save();
+                    DB::commit();
+                    Log::notice('スクレイピングバッチの実行が成功しました。');
 
-        } catch (\Exception $exception) {
-            Log::error('記事の更新に失敗しました', [
-                'exception' => $exception->getMessage(),
-                'file' => __FILE__,
-                'method' => __FUNCTION__,
-                'line' => __LINE__
-            ]);
-            DB::rollBack();
-        }
-    });
+                } catch (\Exception $exception) {
+                    Log::error('AV女優の更新に失敗しました', [
+                    'exception' => $exception->getMessage(),
+                    'file' => __FILE__,
+                    'method' => __FUNCTION__,
+                    'line' => __LINE__
+                    ]);
+                    DB::rollBack();
+                }
+            });
 
-        }       
+        });
 
-    }
+    }       
+ }
 
